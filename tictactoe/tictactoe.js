bc3b893230c3182014398cce6e352d2f1e4e2698
Stephen King <stephen@rocketmade.com>
2016-01-16 15:33:19 -0700

    still WIP

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,40 +1,59 @@
 'use strict';
 
 (function iife() {
-  
   var tds = document.getElementsByTagName('td');
-  var turn = 'X';
+  var currentPlayer = 'X';
+  var board = [['', '', ''], ['', '', ''], ['', '', '']];
+  var win = 0;
   
   (function initializeBoard() {
     for (let i = 0; i < tds.length; i += 1) {
       let td = tds[i];
       td.addEventListener('click', clickHandler);
+      td.index = i;
     }
   })();
+ 
+  function updateBoard(index) {
+    var col = Math.floor(index / 3);
+    var row = Math.floor(index % 3);
+    board[row][col] = currentPlayer;
+  }
   
-  function checkTurn() {
-    if (turn === 'X') {
-      this.innerHTML = 'X';
-      turn = 'O';
-    } else {
-      this.innerHTML = 'O';
-      turn = 'X';
+  function checkForWin(row) {
+    for (let val = 0; val < 3; val += 1) {
+      console.log(row[val]);
+      if (row[val] != currentPlayer || row[val] === '') {
+        win = 0;
+        return;
+      }
+      win += 1;
+      if (win === 3) {
+        console.log('The winner is ' + currentPlayer);
+      }
     }
   }
   
-  function checkIfWinner() {
-    
-    // if there is a winner
-    // alert('Winner');
-    // document.location.reload();
+  function isGameOver() {
+    for (let row = 0; row < 3; row +=1) {
+      checkForWin(board[row]);
+    }
   }
   
   function clickHandler() {
     if (this.value) {
       return
     }
-    checkTurn();
-    checkIfWinner();
+    if (currentPlayer === 'X') {
+      this.innerHTML = 'X';
+      updateBoard(this.index);
+      currentPlayer = 'O';
+    } else {
+      this.innerHTML = 'O';
+      updateBoard(this.index);
+      currentPlayer = 'X';
+    }
+    isGameOver();
   }
 
 })();
