48411bfbea443f9bc5b4225ba8243a5109581732
Stephen King <stephen@rocketmade.com>
2016-01-16 12:40:22 -0700

    checkTurn function

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,36 +1,40 @@
 'use strict';
 
 (function iife() {
-  var tds = document.getElementsByTagName('td');
-  var xo = 'O';
   
-  (function initailizeBoard() {
+  var tds = document.getElementsByTagName('td');
+  var turn = 'X';
+  
+  (function initializeBoard() {
     for (let i = 0; i < tds.length; i += 1) {
       let td = tds[i];
       td.addEventListener('click', clickHandler);
-      td.value = '';
     }
   })();
   
-  function checkGameStatus() {
-    for (let i = 0; i < tds.length; i += 1) {
-      
+  function checkTurn() {
+    if (turn === 'X') {
+      this.innerHTML = 'X';
+      turn = 'O';
+    } else {
+      this.innerHTML = 'O';
+      turn = 'X';
     }
+  }
+  
+  function checkIfWinner() {
+    
+    // if there is a winner
+    // alert('Winner');
+    // document.location.reload();
   }
   
   function clickHandler() {
     if (this.value) {
       return
     }
-    if (xo === 'O') {
-      this.innerHTML = 'X';
-      xo = 'X';
-    } else {
-      this.innerHTML = 'O';
-      xo = 'O';
-    }
-    console.log(this.innerHTML);
-    //checkGameStatus();
+    checkTurn();
+    checkIfWinner();
   }
 
 })();
