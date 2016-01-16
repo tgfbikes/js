c5bc4c327861a16286ed8fcb5857c1cb6ff7292e
Stephen King <stephen@rocketmade.com>
2016-01-16 12:04:35 -0700

    refactored board set up into iife called initializedBoard

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,16 +1,36 @@
 'use strict';
 
 (function iife() {
+  var tds = document.getElementsByTagName('td');
+  var xo = 'O';
   
-  function clickHandler() {
-    console.log('clicked');
-    console.log(this.id);
+  (function initailizeBoard() {
+    for (let i = 0; i < tds.length; i += 1) {
+      let td = tds[i];
+      td.addEventListener('click', clickHandler);
+      td.value = '';
+    }
+  })();
+  
+  function checkGameStatus() {
+    for (let i = 0; i < tds.length; i += 1) {
+      
+    }
   }
   
-  var tds = document.getElementsByTagName('td');
-  for (let i = 0; i < tds.length; i += 1) {
-    let td = tds[i];
-    td.addEventListener('click', clickHandler);
+  function clickHandler() {
+    if (this.value) {
+      return
+    }
+    if (xo === 'O') {
+      this.innerHTML = 'X';
+      xo = 'X';
+    } else {
+      this.innerHTML = 'O';
+      xo = 'O';
+    }
+    console.log(this.innerHTML);
+    //checkGameStatus();
   }
 
 })();
