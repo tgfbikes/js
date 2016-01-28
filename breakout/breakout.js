302f24b6ec222f44621b83012146f6cff8c8e89e
Stephen King <stephen@rocketmade.com>
2016-01-28 14:28:38 -0700

    ball moving in random directions, still need to create boundaries WIP

--- breakout/breakout.js
+++ breakout/breakout.js
@@ -4,15 +4,13 @@
   var main = document.getElementById('main');
   var paddle = document.createElement('div');
   var ball = document.createElement('div');
-  //var intervalID = window.setInterval(moveBall, 20);
+  var intervalID = window.setInterval(moveBall, 20);
   // ball velocity
   var msPerFrame = 20;
   var secondsPerFrame = msPerFrame / 1000;
-
   // This sets horizontal rate to 200--600 pixels/second
   var vx = secondsPerFrame * (Math.floor(Math.random() * 400) + 200);
   if (Math.random() < 0.5) vx = -vx;
-
   // This sets verical rate to 500 pixels/second
   var vy = secondsPerFrame * 500;
   
@@ -23,15 +21,28 @@
       paddle.style.left = 660;
     } else {
       paddle.style.left = evt.clientX - 630;
-      console.log(evt.clientX);
     }
   });
   
   document.body.addEventListener('click', function createBall(evt) {
     ball.id = 'ball';
+    ball.style.left = 300;
+    ball.style.top = 385;
     main.appendChild(ball);
     document.body.removeEventListener('click');
   });
+  
+  function checkForCollision() {
+    
+  }
+  
+  function moveBall() {
+    var oldX = parseInt(ball.style.left, 10);
+    var oldY = parseInt(ball.style.top, 10);
+    ball.style.left = oldX + vx;
+    ball.style.top = oldY + vy;
+    //checkForCollision();
+  }
   
   function setUpBoard() {
     for (let i = 0; i < 10; i += 1) {
@@ -53,7 +64,8 @@
   }
   
   
-  
+  console.log(vx);
+  console.log(vy);
   setUpBoard();
   addPaddle();
   
