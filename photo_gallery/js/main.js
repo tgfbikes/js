c15094e4f559da26c8047b0b81ded1bb9f10fd9b
Stephen King <stephen@rocketmade.com>
2016-02-11 14:30:15 -0700

    thumbnails appending to specific tabs

--- photo_gallery/js/main.js
+++ photo_gallery/js/main.js
@@ -2,8 +2,32 @@
 
 (function pictureGalleryIIFE() {
   
+  function configurePicURL() {
+    let newPicList;
+    for (let catIndex in piclist) {
+      let categoryArray= piclist[catIndex];
+      let fragment = ('#fragment-' + catIndex);
+      for (let imageIndex in categoryArray) {
+        let imagePair = categoryArray[imageIndex];
+        let fullImage = baseurl + imagePair[0];
+        let thumbImage = baseurl + imagePair[1];
+        $(fragment).append('<image src="' + thumbImage +'" >');
+      }
+    }
+  }
+  
+  function setTabs() {
+    $('#tabs').tabs({
+      active: 0
+    });
+  }
+  
+  function addPicsToTabs(newPicList) {
+  }
+  
   $(document).ready(function main() {
-    console.log(piclist);
+    configurePicURL();
+    setTabs();
   }); // document ready
   
 }()); // pictureGalleryIIFE
