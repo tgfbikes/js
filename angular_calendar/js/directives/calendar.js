230b8b4a03b2c9b15b7f075bb45f6ce8ae4aebf6
Stephen King <stephen@rocketmade.com>
2016-04-01 08:19:53 -0600

    fixed typos

--- angular_calendar/js/directives/calendar.js
+++ angular_calendar/js/directives/calendar.js
@@ -5,10 +5,18 @@
     .directive('skCalendar', skCalendar);
 
   function skCalendar() {
-
     return {
       restrict: 'E',
       templateUrl: 'calendar.html',
+      controllerAs: 'vm',
+      controller: function skCalendarController() {
+        var vm = this;
+        vm.changeName = changeName();
+
+        function changeName() {
+          vm.name = "Stephen F. King";
+        }
+      }
     }
   }
 
