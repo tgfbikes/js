445948a36c6f1c9c38dda6d21a203ba5c31333b2
Stephen King <stephen@rocketmade.com>
2016-04-04 09:12:05 -0600

    initial set up of date data

--- angular_calendar/js/dateService.js
+++ angular_calendar/js/dateService.js
@@ -4,10 +4,48 @@
     .module('calendarApp')
     .factory('dateService', dateService);
 
+    dateService.$inject = []
+
     function dateService() {
       var service = {
-        January: {
-          
+        year: 2016,
+        months: {
+          January: {
+            days: 31
+          },
+          February: {
+            days: 28
+          },
+          March: {
+            days: 31
+          },
+          April: {
+            days: 30
+          },
+          May: {
+            days: 31
+          },
+          June: {
+            days: 30
+          },
+          July: {
+            days: 31
+          },
+          August: {
+            days: 31
+          },
+          September: {
+            days: 30
+          },
+          October: {
+            days: 31
+          },
+          November: {
+            days: 30
+          },
+          December: {
+            days: 31
+          }
         }
       }
       return service;
